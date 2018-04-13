24f040f21ce228346deb26a6ce4433a30d4b27f9
Morgan Scofield <mkscofie@Morgans-MacBook-Pro.local>
2018-04-12 21:08:07 -0700

    color working

--- exec/ClickedPoints.js
+++ exec/ClickedPoints.js
@@ -48,6 +48,11 @@ function main() {
   canvas.onmousedown = function(ev){ click(ev, gl, canvas, a_Position); };
   //Register function (event handler) for updating mouse position when moved
   canvas.onmousemove = function(ev){ rubberband(ev, gl, canvas, a_Position); };
+  //Change color of lines on right click mouse release
+  //cbutton.addEventListener("click", colorChange(ev, gl, canvas, a_Position));
+
+  var cbutton = document.getElementById("lineColor");
+  cbutton.onclick = function(ev){ colorChange(ev, gl, canvas, a_Position); };
 
   // Specify the color for clearing <canvas>
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -60,8 +65,10 @@ var g_points = []; // The array for the position of a mouse press
 var prevX;
 var prevY;
 var done = 0; //Boolean for ending rubberband
+var color = 0;
 
 function click(ev, gl, canvas, a_Position) {
+  done = 0;
   var x = ev.clientX; // x coordinate of a mouse pointer
   var y = ev.clientY; // y coordinate of a mouse pointer
   var rect = ev.target.getBoundingClientRect() ;
@@ -84,6 +91,13 @@ function click(ev, gl, canvas, a_Position) {
   // Draw
   gl.drawArrays(gl.POINTS, 0, len);
   gl.drawArrays(gl.LINE_STRIP, 0, len);
+
+  if(ev.button == 2){
+    done = 1;
+    for(i = 0; i < g_points.length; i ++){
+      console.log(g_points[i]);
+    }
+  }
 }
 
 function rubberband(ev, gl, canvas, a_Position){
@@ -109,11 +123,6 @@ function rubberband(ev, gl, canvas, a_Position){
   // Pass the position of a point to a_Position variable
   gl.bufferData(gl.ARRAY_BUFFER, newArr, gl.STATIC_DRAW);
 
-  //var elem = newArr.BYTES_PER_ELEMENT;
-
-  // gl.enableVertexAttribPointer(a_Position, 2, gl.FLOAT, false, BPE * 2);
-  // gl.enableVertexAttribArray(a_Position);
-
   //Draw
   gl.vertexAttrib2f(a_Position, newArr[newArr.length - 2], newArr[newArr.length - 1], 0.0);
   gl.drawArrays(gl.POINTS, 0, len);
@@ -121,6 +130,52 @@ function rubberband(ev, gl, canvas, a_Position){
 
   g_points.pop();
   g_points.pop();
+}
+
+function colorChange(ev, gl, canvas, a_Position){
+ /* if(color % 2 == 0){
+    gl_FragColor = (1.0, 0.0, 1.0, 1.0);
+    color++;
+  }
+  else{
+    gl_FragColor = (1.0, 0.0, 0.0, 1.0);
+    color--;
+  }*/
+  VSHADER_SOURCE =
+    'attribute vec4 a_Position;\n' +
+    'void main() {\n' +
+    '  gl_Position = a_Position;\n' +
+    '  gl_PointSize = 10.0;\n' +
+  '}\n';
+
+  FSHADER_SOURCE =
+    'void main() {\n' +
+    '  gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);\n' +
+  '}\n';
+
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log('Failed to intialize shaders.');
+    return;
+  }
+  var xPos = ev.clientX;
+  var yPos = ev.clientY;
+  var rect = ev.target.getBoundingClientRect();
+
+  xPos = ((xPos - rect.left) - canvas.width/2)/(canvas.width/2);
+  yPos = (canvas.height/2 - (yPos - rect.top))/(canvas.height/2);
+
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  var len = g_points.length / 2;
+  var newArr = new Float32Array(g_points);
+
+  // Pass the position of a point to a_Position variable
+  gl.bufferData(gl.ARRAY_BUFFER, newArr, gl.STATIC_DRAW);
+
+  //Draw
+  gl.drawArrays(gl.POINTS, 0, len);
+  gl.drawArrays(gl.LINE_STRIP, 0, len);
 }
 
 function initVertexBuffers(gl) {
